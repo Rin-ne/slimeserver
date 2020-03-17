06c6c65472e0dea8a539d85d45293bf7e268c664
Albatros <ashja234@gmail.com>
2020-03-17 16:55:11 +0700

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let express = require('express')
 let cors = require('cors')
+let bodyParser = require('body-parser')
 let x = express()
+let { Client } = require('pg');
+
 const PORT = process.env.PORT || 3000;
-const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -11,15 +13,17 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 x.use(cors())
+x.use(bodyParser.json())
+x.use(bodyParser.urlencoded({ extended : true}))
 
 x.all("/", (req, res)=>{
     res.send("Slimechat API V.0.1 Beta --- NO DIRECT ACCESS ALLOWED")
