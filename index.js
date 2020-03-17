f2fd43d546710e18443b9880a4e5dffa72d4c532
Albatros <ashja234@gmail.com>
2020-03-17 16:41:42 +0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ let express = require('express')
 let cors = require('cors')
 let x = express()
 const PORT = process.env.PORT || 3000;
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 x.use(cors())
 
