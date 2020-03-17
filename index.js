7dd896cacb41a2dacaee79561eb192130bdc695a
Albatros <ashja234@gmail.com>
2020-03-17 16:36:05 +0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let express = require('express')
 let cors = require('cors')
 let x = express()
+const PORT = process.env.PORT || 3000;
+
 x.use(cors())
 
 x.all("/", (req, res)=>{
     res.send("Slimechat API V.0.1 Beta --- NO DIRECT ACCESS ALLOWED")
+})
+
+x.listen(PORT, ()=>{
+    console.log("server started on port" + PORT)
 })
