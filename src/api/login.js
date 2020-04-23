2b21027a6f27daee49d1533bffa6146f9d8463cf
illyasviell <ashja234@gmail.com>
2020-04-23 12:18:48 +0700

    commit

--- src/api/login.js
+++ src/api/login.js
@@ -4,7 +4,7 @@ const users = (req, res) => {
   if (req.query.phone) {
     const phone = req.query.phone
     sql.serialize(()=>{
-      sql.get("SELECT * FROM api_users WHERE phoneNumber="+phone, (err, row)=>{
+      sql.get("SELECT * FROM api_users WHERE phoneNumber='"+phone+"'", (err, row)=>{
         if(err || !row){
           res.send("")
         }else{
